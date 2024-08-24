12720d09e9dd95f19399ba8d75714281152d943c
Kyle Bennett <kylebennett00001@gmail.com>
2024-08-24 12:08:17 -0400

    test(big-chair): add unit tests

--- tests/factory/big-chair.test.ts
+++ tests/factory/big-chair.test.ts
@@ -1,10 +1,10 @@
 
-        import { describe, expect, test } from '@jest/globals';
-        // import * as SUT from '';
+import { describe, expect, test, it } from '@jest/globals';
+import BigChair from '../../src/factory/big-chair';
 
-        describe('SYSTEM_UNDER_TEST', () => {
-            test.skip('should do something', () => {
-                expect('SYSTEM_UNDER_TEST').toBeDefined();
-            });
-        });
-    
+describe('BigChair', () => {
+    it('Should return an instance of BigChair with h,w,d of 80,80,80', () => {
+        const BigChairInstance = new BigChair();
+        expect(BigChairInstance.getDimensions()).toEqual({ height: 80, width: 80, depth: 80 });
+    })
+});
