35fc1e6094365d84df63b02e39b089fa6714f815
Kyle Bennett <kylebennett00001@gmail.com>
2024-08-24 11:46:31 -0400

    test(chair.ts): add tests

--- tests/factory/chair.test.ts
+++ tests/factory/chair.test.ts
@@ -1,10 +1,34 @@
 
-        import { describe, expect, test } from '@jest/globals';
-        // import * as SUT from '';
+import { beforeEach, describe, expect, jest, test } from '@jest/globals';
+import Chair from '../../src/factory/chair';
+import type { dimension } from '../../src/factory/dimension';
 
-        describe('SYSTEM_UNDER_TEST', () => {
-            test.skip('should do something', () => {
-                expect('SYSTEM_UNDER_TEST').toBeDefined();
-            });
-        });
-    
+describe('Chair Class', () => {
+    let ChairInstance: Chair;
+
+    beforeEach(() => {
+        ChairInstance = new Chair();
+    })
+
+    test('Have a default of 0 for height', () => {
+        expect(ChairInstance.height).toBe(0);
+    })
+
+    test('Have a default of 0 for width', () => {
+        expect(ChairInstance.width).toBe(0);
+    })
+
+    test('Have a default of 0 for depth', () => {
+        expect(ChairInstance.depth).toBe(0);
+    })
+
+    describe('getDimensions', () => {
+        test('Returns the dimensions of the chair', () => {
+            ChairInstance.height = 10;
+            ChairInstance.width = 20;
+            ChairInstance.depth = 30;
+            expect(ChairInstance.getDimensions()).toEqual({ height: 10, width: 20, depth: 30 });
+        })
+
+    })
+});
