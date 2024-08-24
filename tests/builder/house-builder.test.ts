a0c99f9cf225eb6778748f1ea664a565c4340b45
Kyle Bennett <kylebennett00001@gmail.com>
2024-08-24 17:59:44 -0400

    test(house-builder): write unit test for this module
    - one test for each method

--- tests/builder/house-builder.test.ts
+++ tests/builder/house-builder.test.ts
@@ -1,10 +1,50 @@
 
-        import { describe, expect, test } from '@jest/globals';
-        // import * as SUT from '';
+import { describe, expect, test } from '@jest/globals';
+import HouseBuilder from '../../src/builder/house-builder';
+import House from '../../src/builder/house';
 
-        describe('SYSTEM_UNDER_TEST', () => {
-            test.skip('should do something', () => {
-                expect('SYSTEM_UNDER_TEST').toBeDefined();
-            });
-        });
-    
+describe('HouseBuilder', () => {
+    test('it accepts, no args and sets house to a new House instance', () => {
+        const houseBuilder = new HouseBuilder();
+        expect(houseBuilder.house).toBeInstanceOf(House);
+    })
+
+    describe('setBuildingType', () => {
+        test('it sets the buildingType property of the house instance', () => {
+            const houseBuilder = new HouseBuilder();
+            houseBuilder.setBuildingType('Igloo');
+            expect(houseBuilder.house.buildingType).toBe('Igloo');
+        })
+    })
+
+    describe('setWallMaterial', () => {
+        test('it sets the wallMaterial property of the house instance', () => {
+            const houseBuilder = new HouseBuilder();
+            houseBuilder.setWallMaterial('Ice');
+            expect(houseBuilder.house.wallMaterial).toBe('Ice');
+        })
+    })
+
+    describe('setNumberDoors', () => {
+        test('it sets the doors property of the house instance', () => {
+            const houseBuilder = new HouseBuilder();
+            houseBuilder.setNumberDoors(1);
+            expect(houseBuilder.house.doors).toBe(1);
+        })
+    })
+
+    describe('setNumberWindows', () => {
+        test('it sets the windows property of the house instance', () => {
+            const houseBuilder = new HouseBuilder();
+            houseBuilder.setNumberWindows(1);
+            expect(houseBuilder.house.windows).toBe(1);
+        })
+    })
+
+    describe('getResult', () => {
+        test('it returns the house instance', () => {
+            const houseBuilder = new HouseBuilder();
+            expect(houseBuilder.getResult()).toBeInstanceOf(House);
+        })
+    })
+});
