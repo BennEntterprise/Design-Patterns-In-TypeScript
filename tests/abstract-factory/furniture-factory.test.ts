d5a14906c4812a2041781f9c48c8a1cec9a01bda
Kyle Bennett <kylebennett00001@gmail.com>
2024-08-24 13:28:08 -0400

    feat(furniture-factory): add unit test
    - This first basic pass uses a spy on console log to suppress logs in the output
    and verify the error block has run, expect.objectContaining({foo:'bar'}).
    - This makes GREAT coverage in the modules this calls but also raises the question of
    quality of coverage

--- tests/abstract-factory/furniture-factory.test.ts
+++ tests/abstract-factory/furniture-factory.test.ts
@@ -1,10 +1,43 @@
 
-        import { describe, expect, test } from '@jest/globals';
-        // import * as SUT from '';
+import { afterEach, beforeEach, describe, expect, it, jest, test } from '@jest/globals';
+import FurnitureFactory from '../../src/abstract-factory/furniture-factory';
 
-        describe('SYSTEM_UNDER_TEST', () => {
-            test.skip('should do something', () => {
-                expect('SYSTEM_UNDER_TEST').toBeDefined();
-            });
-        });
-    
+describe('FurnitureFactory', () => {
+    let FurnitureFactoryInstance: FurnitureFactory;
+
+    beforeEach(() => {
+        FurnitureFactoryInstance = new FurnitureFactory();
+        jest.spyOn(console, 'log').mockImplementation(() => undefined);
+    })
+
+    afterEach(() => {
+        jest.restoreAllMocks();
+    })
+
+    it('Contains a static method getFurniture', () => {
+        expect(FurnitureFactory.getFurniture).toBeDefined();
+    })
+
+    describe('getFurniture', () => {
+        it('accepts a string parameter representing a furniture type', () => {
+            expect(FurnitureFactory.getFurniture('SmallChair')).toBeDefined();
+
+            expect(FurnitureFactory.getFurniture('MediumChair')).toBeDefined();
+
+            expect(FurnitureFactory.getFurniture('BigChair')).toBeDefined();
+
+            expect(FurnitureFactory.getFurniture('SmallTable')).toBeDefined();;
+
+            expect(FurnitureFactory.getFurniture('MediumTable')).toBeDefined();
+
+            expect(FurnitureFactory.getFurniture('BigTable')).toBeDefined();
+        })
+
+        it('Will log `No Factory Found` if passed an unrecognizable furniture type', () => {
+            const badFurniture = FurnitureFactory.getFurniture('UnrecognizableFurniture');
+            expect(badFurniture).toBeUndefined();
+            expect(console.log).toHaveBeenCalledWith(expect.objectContaining({ message: 'No Factory Found', stack: expect.any(String) }));
+        })
+
+    })
+});
