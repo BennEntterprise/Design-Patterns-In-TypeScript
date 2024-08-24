e9f9ea39b7e611ae5b8f12d2b9b2c09047a558e8
Kyle Bennett <kylebennett00001@gmail.com>
2024-08-24 12:01:18 -0400

    test(small-chair): add unit test

--- tests/factory/small-chair.test.ts
+++ tests/factory/small-chair.test.ts
@@ -1,10 +1,18 @@
 
-        import { describe, expect, test } from '@jest/globals';
-        // import * as SUT from '';
+import { describe, expect, test, beforeEach } from '@jest/globals';
+import SmallChair from '../../src/factory/small-chair';
+import Chair from '../../src/factory/chair';
 
-        describe('SYSTEM_UNDER_TEST', () => {
-            test.skip('should do something', () => {
-                expect('SYSTEM_UNDER_TEST').toBeDefined();
-            });
-        });
-    
+describe('SmallChair', () => {
+    let SmallChairInstance: SmallChair;
+
+    beforeEach(() => {
+        SmallChairInstance = new SmallChair();
+    })
+
+    test('Returns an instance of chair with h,w, d of 40,40,40 ', () => {
+        expect(SmallChairInstance).toBeInstanceOf(SmallChair);
+        expect(SmallChairInstance).toBeInstanceOf(Chair);
+        expect(SmallChairInstance.getDimensions()).toEqual({ height: 40, width: 40, depth: 40 });
+    });
+});
