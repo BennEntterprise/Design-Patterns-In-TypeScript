0de4f3a1946652906f85274470a485ac8c31b723
Kyle Bennett <kylebennett00001@gmail.com>
2024-08-24 11:41:52 -0400

    remove the coverge by default
    it gets really chatty in the console, so pass the --coverage flag when needed.

--- jest.config.ts
+++ jest.config.ts
@@ -19,10 +19,12 @@ const config: Config = {
   // clearMocks: false,
 
   // Indicates whether the coverage information should be collected while executing the test
-  collectCoverage: true,
+  // collectCoverage: true,
 
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  // collectCoverageFrom: undefined,
+  collectCoverageFrom: [
+    "src/**/*.ts",
+  ],
 
   // The directory where Jest should output its coverage files
   // coverageDirectory: "coverage",
@@ -37,12 +39,12 @@ const config: Config = {
   coverageProvider: "v8",
 
   // A list of reporter names that Jest uses when writing coverage reports
-  // coverageReporters: [
-  //   "json",
-  //   "text",
-  //   "lcov",
-  //   "clover"
-  // ],
+  coverageReporters: [
+    "json",
+    "text",
+    "lcov",
+    "clover"
+  ],
 
   // An object that configures minimum threshold enforcement for coverage results
   // coverageThreshold: undefined,
@@ -124,7 +126,7 @@ const config: Config = {
   // restoreMocks: false,
 
   // The root directory that Jest should scan for tests and modules within
-  rootDir: "./tests",
+  rootDir: "./",
 
   // A list of paths to directories that Jest should use to search for files in
   // roots: [
