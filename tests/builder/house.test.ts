392d44e6583baba45caeded342195538bb2ba70c
Kyle Bennett <kylebennett00001@gmail.com>
2024-08-24 15:30:29 -0400

    test(house): add unit test
    - illustrate the use of expect.stringMatching(regex)
    - combine the above with expect.arrayContaining(arrayOfRegex)
    
    We are testing the output of a string so this is a "fuzzy" way
    to test if the house construction outputs a string describing
    the windows and doors

--- tests/builder/house.test.ts
+++ tests/builder/house.test.ts
@@ -1,10 +1,22 @@
+import { describe, expect, it, test } from '@jest/globals';
+import House from '../../src/builder/house';
 
-        import { describe, expect, test } from '@jest/globals';
-        // import * as SUT from '';
-
-        describe('SYSTEM_UNDER_TEST', () => {
-            test.skip('should do something', () => {
-                expect('SYSTEM_UNDER_TEST').toBeDefined();
-            });
-        });
-    
+describe('House', () => {
+    it('by default will return a house with 0 doors, 0 windows, and no wall material or building type', () => {
+        const house = new House();
+        expect(house.doors).toBe(0);
+        expect(house.windows).toBe(0);
+        expect(house.wallMaterial).toBe('');
+        expect(house.buildingType).toBe('');
+    })
+    it('contains a construction method that returns a string some house details', () => {
+        const house = new House();
+        const expectedKeywordsInOutput = [
+            expect.stringMatching(/window/i),
+            expect.stringMatching(/door/i),
+        ];
+        const constructionOutput = house.construction();
+        expect(typeof constructionOutput).toBe('string');
+        expect(constructionOutput.split(' ')).toEqual(expect.arrayContaining(expectedKeywordsInOutput));
+    })
+});
