46eaf5b0ab559090927600ae24202d90e20cb04b
Kyle Bennett <kylebennett00001@gmail.com>
2024-08-25 13:23:53 -0400

    test(small-chair): add tests for abstract factory small chair

--- tests/abstract-factory/small-table.test.ts
+++ tests/abstract-factory/small-table.test.ts
@@ -1,10 +1,22 @@
 
-        import { describe, expect, test } from '@jest/globals';
-        // import * as SUT from '';
+import { beforeEach, describe, expect, test } from '@jest/globals';
+import SmallChair from '../../src/abstract-factory/small-chair';
+import { Chair } from '../../src/abstract-factory/chair';
 
-        describe('SYSTEM_UNDER_TEST', () => {
-            test.skip('should do something', () => {
-                expect('SYSTEM_UNDER_TEST').toBeDefined();
-            });
-        });
-    
+describe('SmallChair', () => {
+    let SmallChairInstance: SmallChair;
+
+    beforeEach(() => {
+        SmallChairInstance = new SmallChair();
+    })
+
+    test('Returns an instance of chair with h,w, d of 40,40,40 ', () => {
+        expect(SmallChairInstance).toBeInstanceOf(SmallChair);
+        expect(SmallChairInstance).toBeInstanceOf(Chair);
+        expect(SmallChairInstance.getDimensions()).toEqual({ height: 40, width: 40, depth: 40 });
+    });
+
+    test('Returns an instance of chair with name SmallChair', () => {
+        expect(SmallChairInstance.name).toBe('SmallChair');
+    })
+});
