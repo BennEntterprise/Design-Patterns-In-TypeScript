4fa02ba76f14c5a62fe74a177a13e4ae26986c83
Kyle Bennett <kylebennett00001@gmail.com>
2024-08-25 13:28:20 -0400

    test(medium-chair): add tests, nothing new here

--- tests/abstract-factory/medium-chair.test.ts
+++ tests/abstract-factory/medium-chair.test.ts
@@ -1,10 +1,20 @@
 
-        import { describe, expect, test } from '@jest/globals';
-        // import * as SUT from '';
+import { beforeEach, describe, expect, it, test } from '@jest/globals';
+import MediumChair from '../../src/abstract-factory/medium-chair';
 
-        describe('SYSTEM_UNDER_TEST', () => {
-            test.skip('should do something', () => {
-                expect('SYSTEM_UNDER_TEST').toBeDefined();
-            });
-        });
-    
+describe('MediumChair', () => {
+    let MediumChairInstance: MediumChair;
+
+    beforeEach(() => {
+        MediumChairInstance = new MediumChair();
+    })
+
+    it('Should return return an instance of MediumChair with h,w,d of 60,60,60', () => {
+        expect(MediumChairInstance).toBeInstanceOf(MediumChair);
+        expect(MediumChairInstance.getDimensions()).toEqual({ height: 60, width: 60, depth: 60 });
+    });
+
+    it('Should have a name of MediumChair', () => {
+        expect(MediumChairInstance.name).toBe('MediumChair');
+    })
+});
