b86752912c3a1409e5f27a93fe108a9ad493a102
Kyle Bennett <kylebennett00001@gmail.com>
2024-08-25 13:30:07 -0400

    test(big-chair): add basic tests, nothing novel here.wq

--- tests/abstract-factory/big-chair.test.ts
+++ tests/abstract-factory/big-chair.test.ts
@@ -1,10 +1,16 @@
 
-        import { describe, expect, test } from '@jest/globals';
-        // import * as SUT from '';
+import { describe, expect, it, test } from '@jest/globals';
+import BigChair from '../../src/abstract-factory/big-chair';
 
-        describe('SYSTEM_UNDER_TEST', () => {
-            test.skip('should do something', () => {
-                expect('SYSTEM_UNDER_TEST').toBeDefined();
-            });
-        });
-    
+describe('BigChair', () => {
+
+    it('Should return an instance of BigChair with h,w,d of 80,80,80', () => {
+        const BigChairInstance = new BigChair();
+        expect(BigChairInstance.getDimensions()).toEqual({ height: 80, width: 80, depth: 80 });
+    })
+
+    it('Should have a name of BigChair', () => {
+        const BigChairInstance = new BigChair();
+        expect(BigChairInstance.name).toBe('BigChair');
+    })
+});
