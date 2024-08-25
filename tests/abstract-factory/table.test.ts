02c74eb9dc8579e9984eb4c4be21ec48427b0d60
Kyle Bennett <kylebennett00001@gmail.com>
2024-08-25 13:21:50 -0400

    test(table&chair): add basic tests

--- tests/abstract-factory/table.test.ts
+++ tests/abstract-factory/table.test.ts
@@ -1,10 +1,47 @@
 
-        import { describe, expect, test } from '@jest/globals';
-        // import * as SUT from '';
+import { afterEach, beforeEach, describe, expect, it, test } from '@jest/globals';
+import { Table } from '../../src/abstract-factory/table'
 
-        describe('SYSTEM_UNDER_TEST', () => {
-            test.skip('should do something', () => {
-                expect('SYSTEM_UNDER_TEST').toBeDefined();
-            });
+describe('Table', () => {
+
+    let table: Table | undefined = undefined;
+
+    beforeEach(() => {
+        table = new Table();
+    })
+
+    afterEach(() => {
+        table = undefined;
+    })
+
+    it('should create a new instance of Table', () => {
+        expect(table).toBeInstanceOf(Table);
+    });
+
+    it('should have a default name of an empty string', () => {
+        expect(table?.name).toBe('');
+    });
+
+    it('should have a default height of 0', () => {
+        expect(table?.height).toBe(0);
+    });
+
+    it('should have a default width of 0', () => {
+        expect(table?.width).toBe(0);
+    });
+
+    it('should have a default depth of 0', () => {
+        expect(table?.depth).toBe(0);
+    });
+
+    describe('getDimensions', () => {
+        it('should return the dimensions of the table', () => {
+            table = new Table();
+            table.height = 10;
+            table.width = 20;
+            table.depth = 30;
+            expect(table?.getDimensions()).toEqual({ height: 10, width: 20, depth: 30 });
         });
-    
+    });
+
+});
