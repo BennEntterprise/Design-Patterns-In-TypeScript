c44d9b4ab5eda8dbd48e7d121780216b417de44e
Kyle Bennett <kylebennett00001@gmail.com>
2024-08-28 21:07:39 -0400

    refactor(house.test): use setup/teardown hooks

--- tests/builder/house.test.ts
+++ tests/builder/house.test.ts
@@ -1,22 +1,28 @@
-import { describe, expect, it, test } from '@jest/globals';
+import { afterEach, beforeEach, describe, expect, it, test } from '@jest/globals';
 import House from '../../src/builder/house';
 
 describe('House', () => {
+    let house: House | undefined;
+    beforeEach(() => {
+        house = new House();
+    })
+    afterEach(() => {
+        house = undefined;
+    })
+
     it('by default will return a house with 0 doors, 0 windows, and no wall material or building type', () => {
-        const house = new House();
-        expect(house.doors).toBe(0);
-        expect(house.windows).toBe(0);
-        expect(house.wallMaterial).toBe('');
-        expect(house.buildingType).toBe('');
+        expect(house?.doors).toBe(0);
+        expect(house?.windows).toBe(0);
+        expect(house?.wallMaterial).toBe('');
+        expect(house?.buildingType).toBe('');
     })
     it('contains a construction method that returns a string some house details', () => {
-        const house = new House();
         const expectedKeywordsInOutput = [
             expect.stringMatching(/window/i),
             expect.stringMatching(/door/i),
         ];
-        const constructionOutput = house.construction();
+        const constructionOutput = house?.construction();
         expect(typeof constructionOutput).toBe('string');
-        expect(constructionOutput.split(' ')).toEqual(expect.arrayContaining(expectedKeywordsInOutput));
+        expect(constructionOutput?.split(' ')).toEqual(expect.arrayContaining(expectedKeywordsInOutput));
     })
 });
