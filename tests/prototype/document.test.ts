5ebd4ec6795db6a70057233180544f729b34a7c5
Kyle Bennett <kylebennett00001@gmail.com>
2024-08-29 09:48:24 -0400

    test(document): add unit tests for a portion of the class
    Teaching Moments: You can assign originalStringify to JSON.stringify,
    spy on the original stringify so you can use .toHaveBeenCalled(), but
    also mockImplementation to be the originalStringify(json) so that
    you get "true" functionality and the ability to test equaulity.

--- tests/prototype/document.test.ts
+++ tests/prototype/document.test.ts
@@ -1,10 +1,34 @@
 
-        import { describe, expect, test } from '@jest/globals';
-        // import * as SUT from '';
+import { describe, expect, it, jest, test } from '@jest/globals';
+import Document from '../../src/prototype/document'
 
-        describe('SYSTEM_UNDER_TEST', () => {
-            test.skip('should do something', () => {
-                expect('SYSTEM_UNDER_TEST').toBeDefined();
-            });
-        });
-    
+describe('Document', () => {
+
+
+
+    describe('constructor', () => {
+        it('takes a mode and 2d array of numbers', () => {
+            const doc = new Document("stubdoc", [[1, 2, 3], [4, 5, 6]])
+            expect(doc.clone).toBeDefined()
+
+        })
+    })
+
+    describe('clone', () => {
+        test('when given a mode == 2 will return a deep clone of the document using stringify/parse', () => {
+            const originalStringify = JSON.stringify;
+            const originalPars = JSON.parse
+            const parseSpy = jest.spyOn(JSON, 'parse').mockImplementation(str => originalPars(str))
+            const stringifySpy = jest.spyOn(JSON, 'stringify').mockImplementation(j => originalStringify(j))
+
+            const document = new Document('stubdoc', [[1, 2, 3], [4, 5, 6]])
+            const clone = document.clone(2);
+
+            expect(stringifySpy).toHaveBeenCalled()
+            expect(parseSpy).toHaveBeenCalled()
+            expect(clone.name).toBe(document.name);
+            expect(clone.array).toEqual(document.array)
+            expect(clone).toEqual(document)
+        })
+    })
+});
