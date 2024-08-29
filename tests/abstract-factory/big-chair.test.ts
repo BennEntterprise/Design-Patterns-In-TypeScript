d369bb7c109ca83177c3b89e30c2dbc4c07f17a8
Kyle Bennett <kylebennett00001@gmail.com>
2024-08-29 08:37:17 -0400

    nit: clean up the *-chair.test files

--- tests/abstract-factory/big-chair.test.ts
+++ tests/abstract-factory/big-chair.test.ts
@@ -1,5 +1,5 @@
 
-import { describe, expect, it, test } from '@jest/globals';
+import { describe, expect, it } from '@jest/globals';
 import BigChair from '../../src/abstract-factory/big-chair';
 
 describe('BigChair', () => {
--- tests/abstract-factory/small-chair.test.ts
+++ tests/abstract-factory/small-chair.test.ts
@@ -1,10 +1,24 @@
 
-        import { describe, expect, test } from '@jest/globals';
-        // import * as SUT from '';
+import { beforeEach, describe, expect, test } from '@jest/globals';
+import SmallChair from '../../src/abstract-factory/small-chair';
 
-        describe('SYSTEM_UNDER_TEST', () => {
-            test.skip('should do something', () => {
-                expect('SYSTEM_UNDER_TEST').toBeDefined();
-            });
-        });
-    
+describe('SmallChair', () => {
+    let smallChair: SmallChair | undefined;
+
+    beforeEach(() => {
+        smallChair = new SmallChair();
+    })
+
+    test('Should be defined', () => {
+        expect(SmallChair).toBeDefined();
+    });
+
+    test('Should return an instance of SmallChair with h,w,d of 40,40,40', () => {
+        expect(smallChair).toBeInstanceOf(SmallChair);
+        expect(smallChair?.getDimensions()).toEqual({ height: 40, width: 40, depth: 40 });
+    })
+
+    test('Should have a name of SmallChair', () => {
+        expect(smallChair?.name).toBe('SmallChair');
+    });
+});
