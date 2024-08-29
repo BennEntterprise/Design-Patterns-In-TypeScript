bfb1d56b5be025dcc1dc95c04d88a58a1ee895e3
Kyle Bennett <kylebennett00001@gmail.com>
2024-08-29 08:32:49 -0400

    fix: small-table.test actually refernces "Table"
    It was referencing chairs before. Oops.

--- tests/abstract-factory/small-table.test.ts
+++ tests/abstract-factory/small-table.test.ts
@@ -1,22 +1,22 @@
 
 import { beforeEach, describe, expect, test } from '@jest/globals';
-import SmallChair from '../../src/abstract-factory/small-chair';
-import { Chair } from '../../src/abstract-factory/chair';
+import SmallTable from '../../src/abstract-factory/small-table';
+import { Table } from '../../src/abstract-factory/table';
 
 describe('SmallChair', () => {
-    let SmallChairInstance: SmallChair;
+    let SmallTableInstance: SmallTable;
 
     beforeEach(() => {
-        SmallChairInstance = new SmallChair();
+        SmallTableInstance = new SmallTable();
     })
 
     test('Returns an instance of chair with h,w, d of 40,40,40 ', () => {
-        expect(SmallChairInstance).toBeInstanceOf(SmallChair);
-        expect(SmallChairInstance).toBeInstanceOf(Chair);
-        expect(SmallChairInstance.getDimensions()).toEqual({ height: 40, width: 40, depth: 40 });
+        expect(SmallTableInstance).toBeInstanceOf(SmallTable);
+        expect(SmallTableInstance).toBeInstanceOf(Table);
+        expect(SmallTableInstance.getDimensions()).toEqual({ height: 40, width: 40, depth: 40 });
     });
 
-    test('Returns an instance of chair with name SmallChair', () => {
-        expect(SmallChairInstance.name).toBe('SmallChair');
+    test('Returns an instance of chair with name SmallTable', () => {
+        expect(SmallTableInstance.name).toBe('SmallTable');
     })
 });
