755e362a933a57f87b401bdc8106a00f8ba2bbef
Kyle Bennett <kylebennett00001@gmail.com>
2024-08-28 21:29:52 -0400

    test(houseboat-director): write tests for the houseboat.
    After mocking the house builder class, the setup can
    create a mock housebuilder class. Calling the HouseBoatDirector.constuct()
    function should utilize the mock. We can then test the mock to test
    .haveBeenCalledWith for those functions.

--- tests/builder/houseboat-director.test.ts
+++ tests/builder/houseboat-director.test.ts
@@ -1,10 +1,50 @@
 
-        import { describe, expect, test } from '@jest/globals';
-        // import * as SUT from '';
+import { afterEach, beforeEach, describe, expect, jest, test } from '@jest/globals';
+import HouseBoatDirector from '../,,/../../src/builder/houseboat-director'
+import HouseBuilder from '../../src/builder/house-builder';
+import House from '../../src/builder/house';
 
-        describe('SYSTEM_UNDER_TEST', () => {
-            test.skip('should do something', () => {
-                expect('SYSTEM_UNDER_TEST').toBeDefined();
-            });
-        });
-    
+jest.mock('../../src/builder/house-builder', () => {
+    const mockHouseBuilderInstance = {
+        setBuildingType: jest.fn().mockReturnThis(),
+        setWallMaterial: jest.fn().mockReturnThis(),
+        setNumberDoors: jest.fn().mockReturnThis(),
+        setNumberWindows: jest.fn().mockReturnThis(),
+        getResult: jest.fn().mockImplementation(() => new House())
+
+    }
+
+    return {
+        __esModule: true,
+        default: jest.fn(() => mockHouseBuilderInstance),
+        mockHouseBuilderInstance
+    }
+})
+
+describe('HouseBoatDirector', () => {
+    let mockHouseBuilder: HouseBuilder | undefined;
+    let house: House | undefined;
+
+    beforeEach(() => {
+        mockHouseBuilder = new HouseBuilder();
+        house = HouseBoatDirector.construct();
+    })
+
+    afterEach(() => {
+        jest.clearAllMocks()
+    })
+
+    test('has one static method called construct ', () => {
+        expect(HouseBoatDirector).toHaveProperty('construct')
+        expect(typeof HouseBoatDirector.construct).toBe('function')
+    })
+
+    test('it calls HouseBuilder with castle properties', () => {
+        expect(mockHouseBuilder?.setBuildingType).toHaveBeenCalledWith('House Boat')
+        expect(mockHouseBuilder?.setWallMaterial).toHaveBeenCalledWith('Wood')
+        expect(mockHouseBuilder?.setNumberDoors).toHaveBeenCalledWith(6)
+        expect(mockHouseBuilder?.setNumberWindows).toHaveBeenCalledWith(8)
+        expect(mockHouseBuilder?.getResult).toHaveBeenCalled()
+        expect(house).toBeInstanceOf(House);
+    })
+});
